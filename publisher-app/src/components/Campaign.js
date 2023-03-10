f2cc61ce1306d444a77d72a9949d7e78c258c73f
hamzashahab1610 <hamzashahab1610@gmail.com>
2023-03-10 15:18:48 +0500

    feat: send email logic

--- publisher-app/src/components/Campaign.js
+++ publisher-app/src/components/Campaign.js
@@ -1,152 +1,185 @@
-import React from 'react';
-import { Typography, Grid, FormControlLabel, FormGroup, Checkbox, Divider } from '@mui/material';
+import React from "react";
+import {
+	Typography,
+	Grid,
+	FormControlLabel,
+	FormGroup,
+	Checkbox,
+	Divider
+} from "@mui/material";
 import { Button } from "@mui/material";
 
+export default function Campaign({ book, submitNomination }) {
+	var templateParams = {
+		to_name: "Alex",
+		from_name: "Sandy",
+		book_title: "Fall On Your Knees!"
+	};
+	return (
+		<div>
+			<Divider
+				orientation="horizontal"
+				variant="middle"
+				sx={{
+					width: "90%",
+					height: "2px",
+					backgroundColor: "#B31D0C",
+					margin: "0 auto",
+					textAlign: "left",
+					marginBottom: "10px"
+				}}
+			/>
+			<Grid
+				container
+				alignItems="center"
+				spacing={3}
+				sx={{
+					paddingLeft: "50px",
+					width: "80%"
+				}}
+			>
+				{/* col 1 */}
+				<Grid item xs={6}>
+					<Typography
+						variant="h6"
+						align="center"
+						sx={{
+							color: "#B31D0C",
+							fontWeight: "bold"
+						}}
+					>
+						Campaign Enrollment
+					</Typography>
+					<Typography variant="h6" align="center">
+						All Zones
+					</Typography>
+					<FormControlLabel
+						control={
+							<Checkbox
+								sx={{
+									borderRadius: "50%",
+									"&.Mui-checked": {
+										color: "#fff",
+										backgroundColor: "#B31D0C"
+									}
+								}}
+							/>
+						}
+						shape="rounded"
+						label="$80"
+						labelPlacement="bottom"
+					/>
+				</Grid>
 
-export default function Campaign({ book, submitNomination }) {    
-    var templateParams = {
-        to_name: 'Alex',
-        from_name: 'Sandy',
-        book_title: 'Fall On Your Knees!'
-    };
-    return (
-        <div>
-            <Divider
-                orientation="horizontal"
-                variant="middle"
-                sx={{
-                    width: "90%",
-                    height: "2px",
-                    backgroundColor: "#B31D0C",
-                    margin: "0 auto",
-                    textAlign: "left",
-                    marginBottom: "10px",
-                }}
-            />
-            <Grid container alignItems="center" spacing={3}
-                sx={{
-                    paddingLeft: "50px",
-                    width: "80%",
-                }}
-            >
-                {/* col 1 */}
-                <Grid item xs={6}>
-                    <Typography variant="h6" align="center"
-                        sx={{
-                            color: "#B31D0C",
-                            fontWeight: "bold"
-                        }}
-                    >
-                        Campaign Enrollment
-                    </Typography>
-                    <Typography variant="h6" align="center">
-                        All Zones
-                    </Typography>
-                    <FormControlLabel
-                        control={<Checkbox
-                            sx={{
-                                borderRadius: '50%',
-                                '&.Mui-checked': {
-                                    color: '#fff',
-                                    backgroundColor: '#B31D0C',
-                                },
-                            }}
-                        />}
-                        shape="rounded"
-                        label="$80"
-                        labelPlacement="bottom"
-                    />
+				{/* col 2 */}
+				<Grid
+					container
+					item
+					xs={6}
+					direction="column"
+					sx={{
+						ml: 20
+					}}
+				>
+					<FormGroup>
+						<Grid container item direction="column">
+							<FormControlLabel
+								control={
+									<Checkbox
+										sx={{
+											borderRadius: "50%",
+											"&.Mui-checked": {
+												color: "#fff",
+												backgroundColor: "#B31D0C"
+											}
+										}}
+									/>
+								}
+								label="Zone 1 - Rural Central Ontario"
+							/>
+							<FormControlLabel
+								control={
+									<Checkbox
+										sx={{
+											borderRadius: "50%",
+											"&.Mui-checked": {
+												color: "#fff",
+												backgroundColor: "#B31D0C"
+											}
+										}}
+									/>
+								}
+								label="Zone 2 - Northern Alberta"
+							/>
+							<FormControlLabel
+								control={
+									<Checkbox
+										sx={{
+											borderRadius: "50%",
+											"&.Mui-checked": {
+												color: "#fff",
+												backgroundColor: "#B31D0C"
+											}
+										}}
+									/>
+								}
+								label="Zone 3 -Northern Saskatchewan"
+							/>
 
-                </Grid>
+							<FormControlLabel
+								control={
+									<Checkbox
+										sx={{
+											borderRadius: "50%",
+											"&.Mui-checked": {
+												color: "#fff",
+												backgroundColor: "#B31D0C"
+											}
+										}}
+									/>
+								}
+								label="Zone 4 - Rural Central Ontario"
+							/>
+							<FormControlLabel
+								control={
+									<Checkbox
+										sx={{
+											borderRadius: "50%",
+											"&.Mui-checked": {
+												color: "#fff",
+												backgroundColor: "#B31D0C"
+											}
+										}}
+									/>
+								}
+								label="Zone 5 - Rural Newfoundland & Labrador"
+							/>
+						</Grid>
+					</FormGroup>
+				</Grid>
+			</Grid>
 
-             
-                {/* col 2 */}
-                <Grid container item xs={6} direction="column"
-                            sx={
-                                {
-                                    ml: 20,
-                                }
-                            }
-                        >
-                            <FormGroup>
-                                <Grid container item direction="column">
-                                    <FormControlLabel
-                                        control={<Checkbox sx={{
-                                            borderRadius: '50%',
-                                            '&.Mui-checked': {
-                                                color: '#fff',
-                                                backgroundColor: '#B31D0C',
-                                            },
-                                        }} />}
-                                        label="Zone 1 - Rural Central Ontario"
-                                    />
-                                    <FormControlLabel
-                                        control={<Checkbox sx={{
-                                            borderRadius: '50%',
-                                            '&.Mui-checked': {
-                                                color: '#fff',
-                                                backgroundColor: '#B31D0C',
-                                            },
-                                        }} />}
-                                        label="Zone 2 - Northern Alberta"
-                                    />
-                                    <FormControlLabel
-                                        control={<Checkbox sx={{
-                                            borderRadius: '50%',
-                                            '&.Mui-checked': {
-                                                color: '#fff',
-                                                backgroundColor: '#B31D0C',
-                                            },
-                                        }} />}
-                                        label="Zone 3 -Northern Saskatchewan"
-                                    />
-
-                                    <FormControlLabel
-                                        control={<Checkbox sx={{
-                                            borderRadius: '50%',
-                                            '&.Mui-checked': {
-                                                color: '#fff',
-                                                backgroundColor: '#B31D0C',
-                                            },
-                                        }} />}
-                                        label="Zone 4 - Rural Central Ontario"
-                                    />
-                                    <FormControlLabel
-                                        control={<Checkbox sx={{
-                                            borderRadius: '50%',
-                                            '&.Mui-checked': {
-                                                color: '#fff',
-                                                backgroundColor: '#B31D0C',
-                                            },
-                                        }} />}
-                                        label="Zone 5 - Rural Newfoundland & Labrador"
-                                    />
-                                </Grid>
-
-                            </FormGroup>
-                </Grid>
-            </Grid>
-
-            <Button type="submit"
-                variant="contained"
-                sx={{
-                    backgroundColor: "#FF0000",
-                    color: "#FFFFFF",
-                    cursor: "pointer",
-                    "&:hover": {
-                        backgroundColor: "#FF3333",
-                    },
-                    marginBottom: "10px",
-                    marginTop: "10px",
-                    borderRadius: "10px",
-                }}
-            >
-                Submit
-            </Button>
-        </div>
-    );
+			<Button
+				type="submit"
+				onClick={() => submitNomination(templateParams)}
+				variant="contained"
+				sx={{
+					backgroundColor: "#FF0000",
+					color: "#FFFFFF",
+					cursor: "pointer",
+					"&:hover": {
+						backgroundColor: "#FF3333"
+					},
+					marginBottom: "10px",
+					marginTop: "10px",
+					borderRadius: "10px"
+				}}
+			>
+				Submit
+			</Button>
+		</div>
+	);
 }
-
 
 /*
  *         <header className="Header">
--- publisher-app/src/components/Results.js
+++ publisher-app/src/components/Results.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Footer from "./Footer"
+import Footer from "./Footer";
 
-
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import "./Header.css";
 import Header from "./Header";
-import Campaign from "./Campaign"
-import Section1 from "./Section1"
-import LPGMembers from "./LPGMembers"
-import emailjs from '@emailjs/browser';
-
+import Campaign from "./Campaign";
+import Section1 from "./Section1";
+import LPGMembers from "./LPGMembers";
+import emailjs from "@emailjs/browser";
 
 export default function Result({ getBookDetails, setEan, book }) {
-    const { id } = useParams()
-    
+	const { id } = useParams();
+
 	const [error, setError] = useState("");
 
 	const handleSubmit = (event) => {
@@ -28,12 +26,11 @@ export default function Result({ getBookDetails, setEan, book }) {
 		alert(`Handle Submit`);
 		console.log(event.target);
 		console.log(event.target.your);
-	}
+	};
 
 	useEffect(() => {
 		getBookDetails(id);
 	}, []);
-
 
 	function removeHTMLTags(str) {
 		return str.replace(/<[^>]*>/g, "");
@@ -51,29 +48,49 @@ export default function Result({ getBookDetails, setEan, book }) {
 		return str7;
 	}
 
-	const submitNomination = (e, templateParams) => {
+	const submitNomination = (templateParams) => {
 		console.log("here we should send an email");
-		console.log(e.target);
-		e.preventDefault();
-	//	emailjs.send('service_828erdg', 'template_gwo9djb', templateParams, '-ajVzYm2FgRIQHhAT')
-	//		.then((result) => {
-	//			console.log(result.text);
-	//		}, (error) => {
-	//			console.log(error.text);
-	//		});
+		// console.log(e.target);
+		// e.preventDefault();
+
+		// Here we should get the values from the form
+		// and send them to emailjs
+		console.log("book", book);
+
+		// Construct the email subject and body
+		const emailSubject = `LPG Book Nomination: ${book?.Title[0]?.TitleText._text}`;
+		const emailBody = `
+    <p>Publisher Name: ${book?.Publisher?.PublisherName?._text}</p>
+    <p>Imprint Name: ${book?.Imprint?.ImprintName?._text}</p>
+    <p>ISBN: ${book?.RecordReference._text}</p>
+    <p>Price: ${book?.SupplyDetail?.Price[0]?.PriceAmount?._text}</p>
+    <p>Publication Year: ${book?.PublicationDate?._text?.slice(0, 4)}</p>
+    <p>Contributor: ${book?.Contributor?.PersonName?._text}</p>
+    <p>Subject: ${book?.MainSubject?.SubjectHeadingText?._text}</p>
+    <p>Main Description: ${removeAngleBrackets(
+		book?.OtherText[0]?.Text?._text
+	)}</p>
+   `;
+
+		// Send the email
+		//	emailjs.send('service_828erdg', 'template_gwo9djb', templateParams, '-ajVzYm2FgRIQHhAT')
+		//		.then((result) => {
+		//			console.log(result.text);
+		//		}, (error) => {
+		//			console.log(error.text);
+		//		});
 	};
 
 	return (
-        <>
+		<>
 			<Header />
-			<form onSubmit={handleSubmit}>>
+			<form onSubmit={handleSubmit}>
 				<LPGMembers book={book} />
 				<Section1 book={book} />
-				<Campaign book={book} submitNomination={submitNomination} /> 
+				<Campaign book={book} submitNomination={submitNomination} />
 			</form>
 			<Footer />
 			<div id="error-messages">{error && <p>{error}</p>}</div>
-
-            </>
-        )
+		</>
+	);
 }
